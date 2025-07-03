b798b1b3cb59c83e3df32c1d1ddffedd9886414d
Ayodeji Adedeji <ayodejiadedeji8@gmail.com>
2025-07-03 01:52:11 +0100

    Updated Redis configuration

--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -1,10 +1,6 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  // password: process.env.REDIS_PASSWORD
-});
+const redisClient = new Redis(process.env.REDIS_URL);
 
 redisClient.on("connect", () => console.log("🔌 Redis connected"));
 redisClient.on("error", (err) => console.error("❌ Redis error:", err));
