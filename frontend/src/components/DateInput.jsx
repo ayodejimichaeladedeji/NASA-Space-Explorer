fcf0f01298c2d54b7f368a228d3944ddd61f2e60
Ayodeji Adedeji <ayodejiadedeji8@gmail.com>
2025-07-03 13:24:30 +0100

    DateInput fix iOS

--- frontend/src/components/DateInput.jsx
+++ frontend/src/components/DateInput.jsx
@@ -1,24 +1,31 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function DateInput({ id, name, date, onChange, maxDate }) {
   const { isDark } = useContext(ThemeContext);
 
   return (
     <div className="flex flex-col gap-3 sm:gap-2 w-[280px] sm:w-80 sm:min-w-48 mx-auto">
-      <label htmlFor={id} className={`mb-1 text-sm ${isDark ? 'text-white/90' : 'text-black/90'}`}>
+      <label htmlFor={id} className={`mb-1 text-sm ${isDark ? "text-white/90" : "text-black/90"}`}>
         {name}
       </label>
-      <input
-        type="date"
+      <DatePicker
         id={id}
-        max={maxDate}
-        value={date}
-        onChange={(e) => onChange(e.target.value)}
-        onBlur={(e) => onChange(e.target.value)}
+        selected={date ? new Date(date) : null}
+        onChange={(dateObj) => {
+          if (dateObj) {
+            const isoDate = dateObj.toISOString().split("T")[0];
+            onChange(isoDate); // returns "YYYY-MM-DD"
+          }
+        }}
+        maxDate={maxDate ? new Date(maxDate) : null}
         className={`w-full px-2 py-2 sm:py-2 sm:px-4 border rounded-xl bg-white/10 text-base transition-all backdrop-blur-sm
           duration-300 focus:outline-none focus:border-teal-400 focus:ring-3 focus:ring-teal-400/30 disabled:opacity-50 
-          disabled:cursor-not-allowed ${isDark ? 'border-white/20' : 'border-black/20'}`}
+          disabled:cursor-not-allowed ${isDark ? "border-white/20 text-white" : "border-black/20 text-black"}`}
+        dateFormat="yyyy-MM-dd"
+        placeholderText={name}
       />
     </div>
   );
