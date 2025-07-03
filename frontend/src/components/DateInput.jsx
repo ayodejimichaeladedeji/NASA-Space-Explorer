3b05b94810924a49c7834a777096abe8d90de1c0
Ayodeji Adedeji <ayodejiadedeji8@gmail.com>
2025-07-03 13:07:26 +0100

    DateInput iOS

--- frontend/src/components/DateInput.jsx
+++ frontend/src/components/DateInput.jsx
@@ -1,40 +1,26 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function DateInput({ id, name, date, onChange, maxDate }) {
   const { isDark } = useContext(ThemeContext);
-  const [inputType, setInputType] = useState('text');
-
-  function handleFocus(){
-    setInputType('date');
-  };
-
-  function handleBlur(e){
-    if (!e.target.value) {
-      setInputType('text');
-    }
-  };
 
   return (
-    <>
-       <div className="flex flex-col gap-3 sm:gap-2 w-[280px] sm:w-80 sm:min-w-48 mx-auto">
-        <div className="relative">
-          <input
-            type={inputType}
-            id={id}
-            max={maxDate}
-            placeholder={inputType === 'text' ? name : ''}
-            value={date}
-            onChange={(e) => onChange(e.target.value)}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            className={`w-full px-2 py-2 sm:py-2 sm:px-4 border  rounded-xl bg-white/10 text-base transition-all backdrop-blur-sm
-                        duration-300 focus:outline-none focus:border-teal-400 focus:ring-3 focus:ring-teal-400/30 disabled:opacity-50 
-                        disabled:cursor-not-allowed ${isDark ? 'placeholder:text-white/90 border-white/20' : 'placeholder:text-black/90 border-black/20'}`}
-          />
-        </div>
-      </div>
-    </>
+    <div className="flex flex-col gap-3 sm:gap-2 w-[280px] sm:w-80 sm:min-w-48 mx-auto">
+      <label htmlFor={id} className={`mb-1 text-sm ${isDark ? 'text-white/90' : 'text-black/90'}`}>
+        {name}
+      </label>
+      <input
+        type="date"
+        id={id}
+        max={maxDate}
+        value={date}
+        onChange={(e) => onChange(e.target.value)}
+        onBlur={(e) => onChange(e.target.value)}
+        className={`w-full px-2 py-2 sm:py-2 sm:px-4 border rounded-xl bg-white/10 text-base transition-all backdrop-blur-sm
+          duration-300 focus:outline-none focus:border-teal-400 focus:ring-3 focus:ring-teal-400/30 disabled:opacity-50 
+          disabled:cursor-not-allowed ${isDark ? 'border-white/20' : 'border-black/20'}`}
+      />
+    </div>
   );
 }
 
